c46efbc1ca69afa9cfed759c05bd50e448706fb7
Pedro Ramos <pedrorms3225@gmail.com>
2019-01-28 18:33:44 -0200

    Auterando anotações do ID na entidade Planeta (generationType.IDENTITY)

--- workspace-apirest/apirest/src/main/java/apirest/entity/Planeta.java
+++ workspace-apirest/apirest/src/main/java/apirest/entity/Planeta.java
@@ -5,15 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
 public class Planeta {
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idIncremental")
-	@SequenceGenerator(name = "idIncremental")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false, unique = true)
 	private Long id;
 
 	@NotBlank
